4134d92b94894f73a125a384bebfe83e1674bb79
Roberto-Livi <robertomlivi@outlook.com>
2020-08-19 13:11:39 -0400

    finished applying css to login page

--- react-redux/src/components/Login.js
+++ react-redux/src/components/Login.js
@@ -5,12 +5,13 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-
             <form id="form" className="ui form segment">
                 <h1 id="heading">PhotoWorks</h1>
                 <div className="field">
                     <label>Enter Username</label>
-                    <input type="text" name="special-name" />
+                    <input
+                    type="text"
+                    />
                 </div>
                 <div className="ui primary submit button">Submit</div>
                 <div className="ui error message">
